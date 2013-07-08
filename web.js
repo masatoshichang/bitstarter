e2c262e1e8f6514cfe06938766aabc8948361894
masatoshichang <masatoshi.chang@gmail.com>
2013-07-08 07:59:27 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   response.send('Hello World 2!');
+});*/
+
+var fs = require('fs');
+
+fs.readFileSync('index.html', function(err, data){
+    if (err) throw err;
+    console.log(data);
 });
 
 var port = process.env.PORT || 5000;
