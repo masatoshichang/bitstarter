fb4bc185c981fe13ae78beed38136a44b20d01f7
masatoshichang <masatoshi.chang@gmail.com>
2013-07-09 01:48:40 +0000

    Updated web.js from forum

--- web.js
+++ web.js
@@ -2,16 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-/*app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});*/
+app.get('/', function(request, response) {
+    
+    var fs = require('fs');
+    var buffer  new buffer();
+    
+response.send(buffer.toString('utc 8', fs.readFileSync("index.html");
 
-var fs = require('fs');
-
-fs.readFileSync('index.html', function(err, data){
-    if (err) throw err;
-    console.log(data);
+ // response.send('Hello World 2!');
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
