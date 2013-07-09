172e48c424c2d20c2da8a477da935ac191422e17
masatoshichang <masatoshi.chang@gmail.com>
2013-07-09 01:55:17 +0000

    Fixed web.js again

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     
     var fs = require('fs');
-    var buffer  new buffer();
+    var buffer =  new buffer();
     
 response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
 
